719b53be8ac71aa1230f7b977d9558c1857801a6
sabrinaswatee <sabrina.swatee@gmail.com>
2017-07-31 16:11:48 +1000

    commit 4

--- pixart.js
+++ pixart.js
@@ -1,16 +1,24 @@
 var $input = $('#color-field');
 var $button = $('#set-color');
 var $brush = $('.brush');
+var $body = $('body');
 
+// commit 1 and 2
 $button.on('click', function(event) {
   event.preventDefault();
   $brush.css({ 'background-color': $input.val() });
-})
+});
 
-
-var $body = $('body');
+// commit 3
 for (var i = 0; i < 20; i++) {
   var newItem = $('<div>');
   newItem.addClass('square');
   $body.append(newItem);
 };
+
+//commit 4
+$body.on('click', function(event) {
+  if (event.target.className == 'square') {
+    event.target.style.backgroundColor = 'green';
+  };
+});
